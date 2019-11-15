2ab19aed87f9278000d359ecd47ea026b0c892bf
Nathan <nathan.johnson999@aggiemail.usu.edu>
2019-11-15 14:20:09 -0700

    so far

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.event.MouseEvent;
+
 
 public class Main extends Application {
 
-    public void CharacterSelect(){
-
-
+    public FlowPane CharacterSelect(){
 
         Character[] characters =  {
             new BugBoy(),
@@ -30,16 +29,45 @@ public class Main extends Application {
             new TheDollKid(),
             new TheFrogSlayer(),
         };
-
+        BorderPane forbackdrop = new BorderPane();
         FlowPane backdrop = new  FlowPane();
+        forbackdrop.setCenter(backdrop);
+        for (Character i: characters){
+            Button button = new Button("Select " + i.getName() );
+            Button character = new Button(i.getName() + "Description");
+            BorderPane x = new BorderPane();
+            Text text = new Text(
+                i.getName() + "\n" +
+                i.getBio() + "\n" +
+                "Knowledge: " + i.getKnowledge() + "\n" +
+                "Sanity: " + i.getSanity() + "\n" +
+                "Speed: " + i.getSpeed() + "\n" +
+                "Might: " + i.getMight() + "\n"
+            );
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+
+                }
+            });
+
+            x.setCenter(button);
+            x.setBottom(character);
+            backdrop.getChildren().add(x);
+        }
+
+
+
+
+        return backdrop;
 
     }
 
     public void start(Stage stage){
 
-
-
-
+        Scene scene = new Scene(CharacterSelect());
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[]args){
