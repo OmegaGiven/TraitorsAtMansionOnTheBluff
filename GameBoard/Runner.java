3a08082559c3ff00208c9d9f444b161cc6aa0f19
James Richardson <james.richardson@aggiemail.usu.edu>
2019-11-20 23:23:40 -0700

    made some major changes to GameBoard.java. Well, I made the changes to CallBoard and Runner, to make sure that they can be run from the Main that Nathan is working on. Trying to make everything compatible with everything else.

---
+++ GameBoard/Runner.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Runner extends Application {
+
+    public void start(Stage stage) {
+
+        Tile tile1 = new Tile(new ImageView("tiles_images/E.png"), false, false, false, false, true);
+
+        BorderPane pane = new BorderPane();
+        CallBoard board = new CallBoard();
+        Scene scene = new Scene(pane, 1000, 600);
+
+        TheFrogSlayer character = new TheFrogSlayer();
+
+        stage.setScene(scene);
+        board.run(stage, pane, character);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
