114f076440ef6ef97aa75c851a5013680a3cb0a1
James Richardson <james.richardson@aggiemail.usu.edu>
2019-11-24 23:05:41 -0700

    Made it so that when all the tiles run out, you can't explore any new ones and break the program.

--- src/GameBoard/GameBoard.java
+++ src/GameBoard/GameBoard.java
@@ -1,6 +1,8 @@
 package GameBoard;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -8,9 +10,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import Characters.Character;
-
 import java.util.ArrayList;
-import java.util.Collections;
+
 
 public class GameBoard {
 
@@ -38,15 +39,27 @@ public class GameBoard {
     // this variable just keeps track of what pane the player is on right now.S
     private int onPane;
     private Tile[][][] boardTiles = new Tile[3][100][100];
+    private ScrollPane scrollPane = new ScrollPane();
+    boolean move = true;
 
-    public void run(Stage stage, BorderPane pane, Character character) {
+
+    public void run(Stage stage, BorderPane pane1, Character character) {
+
+        this.pane = pane1;
+        scrollPane.setContent(pane.getCenter());
+        pane.setCenter(scrollPane);
+//        Scene scene = new Scene(scrollPane, 1000, 600);
+//        stage.setScene(scene);
+//        stage.show();
+
+
         Tile[] upTiles = {
                 new Tile(new ImageView("GameBoard/tiles_images/E.png"), false, false, false, false, true),
                 new Tile(new ImageView("GameBoard/tiles_images/EW.png"), false, false, false, true, true),
                 new Tile(new ImageView("GameBoard/tiles_images/EWS.png"), false, false, true, true, true),
                 new Tile(new ImageView("GameBoard/tiles_images/N.png"), false, true, false, false, false),
                 new Tile(new ImageView("GameBoard/tiles_images/NE.png"), false, true, false, false, true),
-                new Tile(new ImageView("GameBoard/tiles_images/NES.png"), false, true, true, false, true),
+                new Tile(new ImageView("GameBoard/tiles_images/NES.png"), false, true, true, true, false),
                 new Tile(new ImageView("GameBoard/tiles_images/NEWS.png"), false),
                 new Tile(new ImageView("GameBoard/tiles_images/NS.png"), false, true, true, false, false),
                 new Tile(new ImageView("GameBoard/tiles_images/NW.png"), false, true, false, true, false),
@@ -59,11 +72,10 @@ public class GameBoard {
         };
 
 
-        this.pane = pane;
 
         // this adds the stair tiles to each floor
         for (int j = 0; j < gridPanes.length; j++) {
-
+            // this sets up the basement and upper floor stair tiles.
             Stairs stair =  new Stairs(images[j], true, true, true, true);
             stair.image().setFitWidth(200);
             stair.image().setFitHeight(200);
@@ -73,20 +85,22 @@ public class GameBoard {
 
             // This sets the gridPanes to have the groundHall and the Entrance tiles
             if(j == 1) {
+                // this sets up the stairs for the ground floor
                 stair = new Stairs(images[j], false, false, false, true);
                 stair.image().setFitWidth(200);
                 stair.image().setFitHeight(200);
                 stairs[j] = stair;
                 boardTiles[j][0][5] = stair;
 
-                Tile tile = new Tile(new ImageView("GameBoard/GroundHall.png"),
-                        false, true, true, true, true);
+                // this sets up the middle tile of the ground floor
+                Tile tile = new Tile(new ImageView("GameBoard/GroundHall.png"), false);
                 tile.image().setFitWidth(200);
                 tile.image().setFitHeight(200);
                 boardTiles[j][1][5] = tile;
                 gridPanes[j].add(tile.image(), 1, 5);
-                tile = new Tile(new ImageView("GameBoard/GroundEntrance.png"),
-                        false, true, true, true, true);
+
+                // this sets up the far right tile of the ground floor
+                tile = new Tile(new ImageView("GameBoard/GroundEntrance.png"), false);
                 tile.image().setFitWidth(200);
                 tile.image().setFitHeight(200);
                 boardTiles[j][2][5] = tile;
@@ -96,7 +110,9 @@ public class GameBoard {
             // adds all the buttons to change between floors
             buttons[j] = new Button(titles[j]);
             int index = j;
-            buttons[j].setOnMouseClicked(event -> pane.setCenter(gridPanes[index]));
+//            buttons[j].setOnMouseClicked(event -> pane.setCenter(gridPanes[index]));
+            buttons[j].setOnMouseClicked(event -> scrollPane.setContent(gridPanes[index]));
+
         }
 
 
@@ -109,6 +125,7 @@ public class GameBoard {
 
         gridPanes[1].add(character.getImage(), character.getX(), character.getY());
 
+        // add the up and down buttons for the right side of the pane.
         rightPane.getChildren().add(up);
         rightPane.getChildren().add(down);
         pane.setRight(rightPane);
@@ -117,52 +134,86 @@ public class GameBoard {
         ArrayList<Integer> added = new ArrayList<>();
         stage.getScene().setOnKeyPressed(event -> {
             int choice = (int)(Math.random() * upTiles.length);
-            while (added.contains(choice)) {
+            while (added.contains(choice) && added.size() != upTiles.length) {
                 choice = (int)(Math.random() * upTiles.length);
             }
-            if (gridPanes[onPane] == pane.getCenter()/* && character.move >= 0*/) {
-//                Collections.shuffle(upTiles);
+            if (gridPanes[onPane] == scrollPane.getContent()/*pane.getCenter()*//* && character.move >= 0*/) {
 
+                // this is the part where it checks for wasd for the movement of the character.
                 boolean door = false;
                 switch (event.getCode()) {
                     case W:
                         if(boardTiles[onPane][character.getX()][character.getY()].nDr) {
                             door = true;
-                            character.setY(character.getY() - 1);
+                            if(move) {
+                                character.setY(character.getY() - 1);
+                            }
                         }
                         break;
                     case S:
                         if(boardTiles[onPane][character.getX()][character.getY()].sDr) {
                             door = true;
-                            character.setY(character.getY() + 1);
+                            if(move) {
+                                character.setY(character.getY() + 1);
+                            }
                         }
                         break;
                     case A:
                         if(boardTiles[onPane][character.getX()][character.getY()].wDr) {
                             door = true;
-                            character.setX(character.getX() - 1);
+                            if(move) {
+                                character.setX(character.getX() - 1);
+                            }
                         }
                         break;
                     case D:
                         if(boardTiles[onPane][character.getX()][character.getY()].eDr) {
                             door = true;
-                            character.setX(character.getX() + 1);
+                            if(move) {
+                                character.setX(character.getX() + 1);
+                            }
                         }
                         break;
                 }
-                GridPane center = (GridPane) pane.getCenter();
+                GridPane center = (GridPane) scrollPane.getContent()/*pane.getCenter()*/;
                 if(door) {
+                    // this is the part where it adds a new tile.
                     if(boardTiles[onPane][character.getX()][character.getY()] == null) {
-                        added.add(choice);
-                        upTiles[choice].image().setFitHeight(200);
-                        upTiles[choice].image().setFitWidth(200);
-                        center.add(upTiles[choice].image(), character.getX(), character.getY());
-                        boardTiles[onPane][character.getX()][character.getY()] = upTiles[choice];
+                        if(added.size() == upTiles.length) {
+                            switch (event.getCode()){
+                                case W:
+                                    character.setY(character.getY() + 1);
+                                    break;
+                                case S:
+                                    character.setY(character.getY() - 1);
+                                    break;
+                                case A:
+                                    character.setX(character.getX() + 1);
+                                    break;
+                                case D:
+                                    character.setX(character.getX() - 1);
+                                    break;
+                            }
+                            move = false;
+                        }
+                        else {
+                            added.add(choice);
+                            upTiles[choice].image().setFitHeight(200);
+                            upTiles[choice].image().setFitWidth(200);
+                            center.add(upTiles[choice].image(), character.getX(), character.getY());
+                            boardTiles[onPane][character.getX()][character.getY()] = upTiles[choice];
+                            move = true;
+                        }
                     }
-
-                    center.getChildren().remove(character.getImage());
-                    center.add(character.getImage(), character.getX(), character.getY());
-                    character.setMoveCount(character.getMoveCount() - 1);
+                    else {
+                        move = true;
+                    }
+                    if (move) {
+                        // this is the part that moves the character.
+                        center.getChildren().remove(character.getImage());
+                        center.add(character.getImage(), character.getX(), character.getY());
+                        character.setMoveCount(character.getMoveCount() - 1);
+                    }
                 }
             }
 
@@ -172,7 +223,7 @@ public class GameBoard {
         up.setOnMouseClicked(e -> {
             if (character.getY() == 5 && character.getX() == 0) {
                 for (int i = 0; i < gridPanes.length; i++) {
-                    if (pane.getCenter() == gridPanes[i]) {
+                    if (/*pane.getCenter()*/scrollPane.getContent() == gridPanes[i]) {
                         if(setStairs(true, character, i)){
                             break;
                         }
@@ -185,7 +236,7 @@ public class GameBoard {
         down.setOnMouseClicked(el -> {
             if (character.getY() == 5 && character.getX() == 0) {
                 for (int i = 0; i < gridPanes.length; i++) {
-                    if (pane.getCenter() == gridPanes[i]) {
+                    if (scrollPane.getContent()/*pane.getCenter()*/ == gridPanes[i]) {
                         if(setStairs(false, character, i)) {
                             break;
                         }
@@ -195,7 +246,12 @@ public class GameBoard {
         });
 
         onPane = 1;
-        pane.setCenter(gridPanes[1]);
+//        pane.setCenter(gridPanes[1]);
+        scrollPane.setContent(gridPanes[1]);
+
+//        ScrollBar scroll = new ScrollBar();
+//        pane.getChildren().add(scroll);
+
 
     }
 
@@ -211,7 +267,8 @@ public class GameBoard {
             }
 
             gridPanes[index].add(character.getImage(), 0, 5);
-            pane.setCenter(gridPanes[index]);
+//            pane.setCenter(gridPanes[index]);
+            scrollPane.setContent(gridPanes[index]);
             return true;
         }
         return false;
